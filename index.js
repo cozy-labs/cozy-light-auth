b5d9ecce52b9cf3fc48b87de8b3dc3fd6c932979
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-08-27 01:03:16 +0200

    configure passport

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-var auth = require('http-auth');
 var fs = require('fs');
 var read = require('read');
 var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 
 
-var basic = auth.basic({
-  realm: "Cozy Light"
-  }, function (username, password, callback) {
+passport.use(new LocalStrategy(
+  function(username, password, callback) {
     bcrypt.compare(password, module.exports.hash, function(err, res) {
-      callback(username === "me" && res);
+      callback(res);
     });
   }
-);
+));
 
+passport.serializeUser(function(user, done) {
+  done(null, 'config');
+});
+
+passport.deserializeUser(function(id, done) {
+  done(err, module.exports.config);
+});
 
 var setPassword = function() {
   var config = module.exports.config;
@@ -28,7 +37,48 @@ var setPassword = function() {
 
 
 module.exports.configureAppServer = function(app, config, routes, callback) {
-  app.use(auth.connect(basic));
+  app.use(cookieParser(randomstring.generate());
+  app.use(cookieSession({
+    secret: randomstring.generate(),
+    maxage: 1000 * 60 * 60 * 24 * 7
+  });
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect("/login");
+    }
+  });
+
+  app.get('/login', function (req, res, next) {
+    res.send('
+<html>
+<head>
+  <title>Cozy Light Log In</title>
+</head>
+<body> \
+  <p>Please give your password to log in:</p> \
+  <form action="/login" method="post"> \
+    <div> \
+        <label>Password:</label> \
+        <input type="password" name="password"/> \
+    </div> \
+    <div> \
+        <input type="submit" value="Log In"/> \
+    </div> \
+  </form> \
+</body>
+</html>
+    ');
+  });
+
+  app.post('/login',
+    passport.authenticate('local', { successRedirect: '/',
+                                     failureRedirect: '/login',
+                                     failureFlash: true });
+
   callback();
 };
 
